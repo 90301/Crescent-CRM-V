7e10ec8db6ae4c65b6bb3ad1ce809bbcf88b4126
Joshua Benton <josh.benton.ce@gmail.com>
2016-09-27 18:39:57 -0400

    user data holder class created

---
+++ Crescent-CRM-Vaadin-Maven/src/main/java/clientInfo/UserDataHolder.java
@@ -0,0 +1,13 @@
+package clientInfo;
+
+public class UserDataHolder {
+
+	/**
+	 * Holds data which is user speciific. Will implement data structures and be a sub
+	 * section of the dataholder class.
+	 */
+	public UserDataHolder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
