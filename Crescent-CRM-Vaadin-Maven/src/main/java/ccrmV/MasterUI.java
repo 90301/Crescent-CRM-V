517d61c9f7eca2a52d3ab7de0814fc500ff73789
Joshua Benton <josh.benton.ce@gmail.com>
2016-10-17 18:27:38 -0400

    scheduler event example

--- Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
@@ -27,8 +27,8 @@ public class MasterUI extends UI {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public static final double versionNumber = .80;
-	public static final String versionDescription = " Filter Improved";
+	public static final double versionNumber = .81;
+	public static final String versionDescription = " Calender Event Example";
 
 	public MasterUI() {
 		// TODO Auto-generated constructor stub
@@ -60,7 +60,7 @@ public class MasterUI extends UI {
 	//if something breaks because of developer mode, turn developer mode off.
 	//no error checking will be implemented for developer mode, if you don't know
 	//exactly what you are doing with a setting, ask someone who does, or don't enable it.
-	public static final Boolean DEVELOPER_MODE = true;
+	public static final Boolean DEVELOPER_MODE = false;
 	//auto login will be enabled if set to true, will attempt to login with DEV_AUTOLOGIN_USER
 	//if no such user exists, the application will crash.
 	public static final Boolean DEV_AUTO_LOGIN = true;
