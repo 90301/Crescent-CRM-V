54a302b4f66e3cf1e3a15b0d68a766ed8dcb6b45
Mark Layton <iris4@live.com>
2016-11-28 14:13:40 -0500

    Added Dark Theme capability, working. Can switch back and forth between dark/light themes

--- Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
@@ -73,7 +73,7 @@ public class MasterUI extends UI {
 	public static final Boolean DEVELOPER_MODE = true;
 	//auto login will be enabled if set to true, will attempt to login with DEV_AUTOLOGIN_USER
 	//if no such user exists, the application will crash.
-	public static final Boolean DEV_AUTO_LOGIN = false;
+	public static final Boolean DEV_AUTO_LOGIN = true; //TODO
 	public static final String DEV_AUTOLOGIN_USER = "ccrmUser";
 	public static final Boolean DEV_TEST_CODE = true;
 	
@@ -101,6 +101,8 @@ public class MasterUI extends UI {
 	public UserDataHolder userDataHolder;//Set when logging in
 	
 	protected void init(VaadinRequest request) {
+		
+		this.setTheme("darkTheme");
 		
 		DataHolder.initalizeDatabases();
 		
