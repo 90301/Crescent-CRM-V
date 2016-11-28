a8c36c893f9db8504cfe3bef65e798725c3c3bef
Tony Simmons <tsiking18@gmail.com>
2016-11-28 14:02:00 -0500

    Added front desk mode [incomplete]

--- Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/SchedulerView.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/SchedulerView.java
@@ -9,6 +9,7 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
+import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.shared.ui.calendar.DateConstants;
@@ -48,8 +49,15 @@ public class SchedulerView extends HorizontalLayout implements View {
 	public NavBar navBar;
 	
 	
+	
 	public HorizontalLayout createEventLayout;
 	public VerticalLayout schedulerLayout;
+	public VerticalLayout frontDeskLayout;
+    public VerticalLayout stylist1Layout;	
+    public VerticalLayout stylist2Layout;	
+    public VerticalLayout stylist3Layout;	
+
+	
 	
 	/*
 	 * UI Components
@@ -60,6 +68,11 @@ public class SchedulerView extends HorizontalLayout implements View {
 	private DateField createEventStartDateField = new PopupDateField("Event Start");
 	private DateField createEventEndDateField = new PopupDateField("Event End");
 	private ComboBox repeatComboBox = new ComboBox("Repeat");
+	private Button switchToFrontDeskMode = new Button("Front Desk Mode");
+	
+	private ComboBox stylistComboBox =  new ComboBox("Stylist 1");
+	private ComboBox stylistComboBox2 =  new ComboBox("Stylist 2");
+	private ComboBox stylistComboBox3 =  new ComboBox("Stylist 3");
 	
 	/* replaced by createEventEndDateField
 	private TextField createEventDurationTextField  = new TextField("Duration");
@@ -67,7 +80,6 @@ public class SchedulerView extends HorizontalLayout implements View {
 	*/
 	
 	private Button createEventButton  = new Button("Create Event");
-	
 	
 	public static final ArrayList<String> baseTimeList = new ArrayList<>();
 	
@@ -80,6 +92,10 @@ public class SchedulerView extends HorizontalLayout implements View {
 		
 		schedulerLayout = new VerticalLayout();
 		createEventLayout = new HorizontalLayout();
+		frontDeskLayout = new VerticalLayout();
+		stylist1Layout = new VerticalLayout();
+		stylist2Layout = new VerticalLayout();
+		stylist3Layout = new VerticalLayout();
 		createEventLayout.setDefaultComponentAlignment(Alignment.BOTTOM_CENTER);
 		/*
 		 * create components here and edit settings
@@ -105,6 +121,60 @@ public class SchedulerView extends HorizontalLayout implements View {
         
         //createEventClientComboBox =
         
+        // Creates the first monthly calendar
+        
+        Calendar cal2 = new Calendar();
+        cal2.setHeight("200px");
+		cal2.setWidth("200px");
+		cal2.setCaption("Current Month");
+		cal2.setLocale(Locale.getDefault());
+        cal2.setImmediate(true);
+        
+        GregorianCalendar gCal = new GregorianCalendar(cal2.getLocale());
+        
+        final int rollAmount = gCal.get(GregorianCalendar.DAY_OF_MONTH) - 1;
+        gCal.add(GregorianCalendar.DAY_OF_MONTH, -rollAmount);
+    //    resetTime(false);
+        Date currentMonthsFirstDate = gCal.getTime();
+        cal2.setStartDate(currentMonthsFirstDate);
+        gCal.add(GregorianCalendar.MONTH, 1);
+        gCal.add(GregorianCalendar.DATE, -1);
+        cal2.setEndDate(gCal.getTime());
+        
+        
+        
+        Calendar cal3 = new Calendar();
+        cal3.setHeight("400px");
+		cal3.setWidth("400px");
+		cal3.setCaption("Current Month");
+		cal3.setLocale(Locale.getDefault());
+        cal3.setImmediate(true);
+        
+        GregorianCalendar gCal3 = new GregorianCalendar(cal2.getLocale());
+        
+        final int rollAmount3 = gCal.get(GregorianCalendar.DAY_OF_MONTH) - 1;
+        gCal3.add(GregorianCalendar.DAY_OF_MONTH, -rollAmount3);
+    //    resetTime(false);
+        Date currentMonthsFirstDate3 = gCal3.getTime();
+        cal3.setStartDate(currentMonthsFirstDate3);
+        gCal3.add(GregorianCalendar.MONTH, 1);
+        gCal3.add(GregorianCalendar.DATE, -1);
+        cal3.setEndDate(gCal3.getTime());
+        
+        Calendar cal4 = new Calendar();
+        cal4.setHeight("400px");
+		cal4.setWidth("400px");
+		cal4.setCaption("Current Month");
+		cal4.setLocale(Locale.getDefault());
+        cal4.setImmediate(true);
+        
+        
+        Calendar cal5 = new Calendar();
+        cal5.setHeight("400px");
+		cal5.setWidth("400px");
+		cal5.setCaption("Current Month");
+		cal5.setLocale(Locale.getDefault());
+        cal5.setImmediate(true);
         
         //date selection
         //createEventStartDateField = ;
@@ -124,6 +194,9 @@ public class SchedulerView extends HorizontalLayout implements View {
         createEventStartDateField.setResolution(Resolution.MINUTE);
         createEventEndDateField.setResolution(Resolution.MINUTE);
        
+        
+        
+        
 
         //Used for weekly view
         cal.setHandler(new BasicDateClickHandler() 
@@ -164,6 +237,46 @@ public class SchedulerView extends HorizontalLayout implements View {
           });
         
         
+        
+      //Used for weekly view
+        cal2.setHandler(new BasicDateClickHandler() 
+        {
+            public void dateClick(DateClickEvent event) 
+            {
+              Calendar cal2 = event.getComponent();
+
+              // Check if the current range is already one day long
+              long currentCalDateRange2 = cal2.getEndDate().getTime() - cal2.getStartDate().getTime();
+
+              // From one-day view, zoom out to week view
+              if (currentCalDateRange2 <= DateConstants.DAYINMILLIS) 
+              {
+                  // Change the date range to the current week
+                  GregorianCalendar weekstart2 = new GregorianCalendar();
+                  GregorianCalendar weekend2   = new GregorianCalendar();
+                  weekstart2.setTime(event.getDate());
+                  weekend2.setTime(event.getDate());
+                  weekstart2.setFirstDayOfWeek(java.util.Calendar.SUNDAY);
+                  weekstart2.set(java.util.Calendar.HOUR_OF_DAY, 0);
+                  weekstart2.set(java.util.Calendar.DAY_OF_WEEK,
+                               java.util.Calendar.SUNDAY);
+                  weekend2.set(java.util.Calendar.HOUR_OF_DAY, 23);
+                  weekend2.set(java.util.Calendar.DAY_OF_WEEK,
+                               java.util.Calendar.SATURDAY);
+                  cal2.setStartDate(weekstart2.getTime());
+                  cal2.setEndDate(weekend2.getTime());
+
+                  Notification.show("Custom zoom to week");
+                  
+              } else 
+              	{
+            	  // Default behavior, change date range to one day
+            	  super.dateClick(event);
+              	}
+            }
+          });
+        
+        
         //createEventDurationTextField
         //createEventDurationTextField.setWidth("80px");
         
@@ -172,6 +285,7 @@ public class SchedulerView extends HorizontalLayout implements View {
         
         //createEventButton
         createEventButton.addClickListener(click -> createEventButtonClick());
+        switchToFrontDeskMode.addClickListener(click -> switchToFrontDeskModeClick());
         
         createEventLayout.addComponent(createEventNameTextField);
         createEventLayout.addComponent(createEventClientComboBox);
@@ -179,9 +293,15 @@ public class SchedulerView extends HorizontalLayout implements View {
         createEventLayout.addComponent(createEventEndDateField);
         createEventLayout.addComponent(repeatComboBox);
         createEventLayout.addComponent(createEventButton);
+        createEventLayout.addComponent(switchToFrontDeskMode);
         
         
-        
+        stylist1Layout.addComponent(stylistComboBox);
+        stylist2Layout.addComponent(stylistComboBox2);
+        stylist3Layout.addComponent(stylistComboBox3);
+   //     fdHLayout.addComponent(cal3);
+//        fdHLayout.addComponent(cal4);
+ //       fdHLayout.addComponent(cal5);
         
         populateComboBoxes();
         
@@ -196,8 +316,20 @@ public class SchedulerView extends HorizontalLayout implements View {
 		schedulerLayout.addComponent(createEventLayout);
 		schedulerLayout.addComponent(cal);
 		
-		this.addComponent(schedulerLayout);
+//		frontDeskLayout.addComponent(schedulerLayout);
+//		frontDeskLayout.addComponent(cal);
+		frontDeskLayout.addComponent(cal2);
 		
+		stylist1Layout.addComponent(cal3);
+		stylist2Layout.addComponent(cal4);
+		stylist3Layout.addComponent(cal5);
+
+		
+		this.addComponent(schedulerLayout);
+		this.addComponent(frontDeskLayout);
+		this.addComponent(stylist1Layout);
+		this.addComponent(stylist2Layout);
+		this.addComponent(stylist3Layout);
 	}
 
 
@@ -234,6 +366,23 @@ public class SchedulerView extends HorizontalLayout implements View {
 	}
 
 
+	private void switchToFrontDeskModeClick(){
+		/**
+		frontDeskLayout.addComponent(createEventNameTextField);
+		frontDeskLayout.addComponent(createEventClientComboBox);
+        frontDeskLayout.addComponent(createEventStartDateField);
+        frontDeskLayout.addComponent(createEventEndDateField);
+        frontDeskLayout.addComponent(repeatComboBox);
+        frontDeskLayout.addComponent(createEventButton);
+        frontDeskLayout.addComponent(switchToFrontDeskMode);
+        **/
+		
+		//frontDeskLayout.addComponent(cal);
+		createEventLayout.setVisible(false);
+		schedulerLayout.setVisible(false);
+		frontDeskLayout.setVisible(true);
+	}
+	
 
 	private void populateComboBoxes() {
 		
