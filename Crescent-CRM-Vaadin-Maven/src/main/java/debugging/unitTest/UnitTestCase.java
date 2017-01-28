6ae484bc2768985f5fcef64936cbd9c68c9994b0
Mark Layton <iris4@live.com>
2017-01-28 18:31:45 -0500

    Started work on test case framework [not functional yet]

---
+++ Crescent-CRM-Vaadin-Maven/src/main/java/debugging/unitTest/UnitTestCase.java
@@ -0,0 +1,172 @@
+package debugging.unitTest;
+
+/**
+ * Data holder for the result of a test
+ * Should also contain utility functions for outputting data.
+ * @author Josh Benton with help from Mark Layton
+ *
+ */
+public class UnitTestCase {
+	public static final String TEST_SUCCESS_STRING = "[✔]";
+	public static final String TEST_FAIL_STRING = "[X]";
+	public static final String DELIMITER = " | ";
+	//Test type constants
+	
+	public static final String TEST_TYPE_OBJECT = "object";
+	public static final String TEST_TYPE_STRING_CONTAINS = "string contains";
+	
+	
+	
+	private String testName = "";
+	private String testMessage = "";
+
+	
+	private Object expectedResult;
+	private Object actualResult;
+	
+	
+	private Boolean testResult = false;
+	//Choosing tests
+	
+	private String testType = "";
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * This is designed to be used to set up a number of tests without
+	 * entering the actual result.
+	 * @param testName
+	 * @param testMessage
+	 * @param expectedResult
+	 * @param testType
+	 */
+	public UnitTestCase(String testName, String testMessage, Object expectedResult, String testType) {
+		super();
+		this.testName = testName;
+		this.testMessage = testMessage;
+		this.expectedResult = expectedResult;
+		this.testType = testType;
+	}
+
+
+	/**
+	 * This Constructor handles everything and simply needs to have the result output
+	 * @param testName
+	 * @param testMessage
+	 * @param expectedResult
+	 * @param actualResult
+	 * @param testType
+	 */
+	public UnitTestCase(String testName, String testMessage, Object expectedResult,
+			Object actualResult, String testType) {
+		super();
+		this.testMessage = testMessage;
+		this.testName = testName;
+		this.expectedResult = expectedResult;
+		this.actualResult = actualResult;
+		this.testType = testType;
+		
+		this.checkTest();
+	}
+	
+	
+	
+	public Boolean getTestResult() {
+		return testResult;
+	}
+	public void setTestResult(Boolean testSuccess) {
+		this.testResult = testSuccess;
+	}
+	public String getTestMessage() {
+		return testMessage;
+	}
+	public void setTestMessage(String testMessage) {
+		this.testMessage = testMessage;
+	}
+	
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+		public Object getExpectedResult() {
+		return expectedResult;
+	}
+	public void setExpectedResult(Object expectedResult) {
+		this.expectedResult = expectedResult;
+	}
+	public Object getActualResult() {
+		return actualResult;
+	}
+	
+	/**
+	 * This adds the actual result to the test case and then checks the result.
+	 * @param actualResult
+	 */
+	public void setActualResult(Object actualResult) {
+		this.actualResult = actualResult;
+		this.checkTest();
+	}
+	
+	/*
+	 * Check results
+	 */
+	
+	/**
+	 * This determines the test to use, and runs that method
+	 * to set the testResult.
+	 * if nothing is set, it defaults to the object test
+	 */
+	public void checkTest() {
+		if (testType.equals(TEST_TYPE_STRING_CONTAINS)) {
+			checkStringContains();
+		} else if (testType.equals(TEST_TYPE_OBJECT)) {
+			checkObjectResult();
+		} else {
+			checkObjectResult();
+		}
+	}
+	
+	
+	/**
+	 * Checks the results of any object that "Equals" will work for.
+	 */
+	public void checkObjectResult() {
+		if (expectedResult.equals(actualResult)) {
+			this.testResult = true;
+		} else {
+			this.testResult = false;
+		}
+	}
+	
+	public void checkStringContains() {
+		if (((String) actualResult).contains(((String)expectedResult))) {
+			this.testResult = true;
+		} else {
+			this.testResult = false;
+		}
+	}
+	/*
+	 * OUTPUT CODE
+	 */
+	public String toString() {
+		String output = "";
+		if (testResult) {
+			output += TEST_SUCCESS_STRING;
+		} else {
+			output += TEST_FAIL_STRING;
+		}
+		
+		output += testName + DELIMITER + testMessage;
+		return output;
+		
+	}
+
+	
+}
---
+++ Crescent-CRM-Vaadin-Maven/src/main/java/debugging/unitTest/UnitTestSuite.java
@@ -0,0 +1,13 @@
+package debugging.unitTest;
+
+/**
+ * Holds Unit Test Cases
+ * These can be executed via "test case executers"
+ * 
+ * @author Josh Benton / With help from Mark Layton
+ *
+ */
+public class UnitTestSuite {
+	
+
+}
