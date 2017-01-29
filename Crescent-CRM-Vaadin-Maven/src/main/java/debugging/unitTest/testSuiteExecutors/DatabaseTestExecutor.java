810446973bcb945da06fbf9d54d65b1be442ed79
Mark Layton <iris4@live.com>
2017-01-29 17:27:07 -0500

    added status test

--- Crescent-CRM-Vaadin-Maven/src/main/java/debugging/unitTest/testSuiteExecutors/DatabaseTestExecutor.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/debugging/unitTest/testSuiteExecutors/DatabaseTestExecutor.java
@@ -2,6 +2,7 @@ package debugging.unitTest.testSuiteExecutors;
 
 import clientInfo.Client;
 import clientInfo.DataHolder;
+import clientInfo.Status;
 import clientInfo.UserDataHolder;
 import debugging.unitTest.TestSuiteExecutor;
 import debugging.unitTest.UnitTestCase;
@@ -13,11 +14,18 @@ import inventory.InventoryItem;
  *
  */
 public class DatabaseTestExecutor extends TestSuiteExecutor {
+	//inventory items testing
 	static String inventoryItem1TestName = "7RR red dye";
 	static String inventoryItemTestKey = "RR2016";
 	static String inventoryItem1TestCategory = "dye";
 	static String inventoryItem1TestBarcode = "0000001";
 	static String inventoryItem1URL = "www.google.com";
+	static int inventoryItem1Stock = 12;
+	static int inventoryItem1ReorderPoint = 6;
+	
+	//status testing
+	static String statusTest1Name = "Prospect";  
+	static int statusTest1Color = 122;
 	
 	@Override
 	public Boolean runTests() {
@@ -41,11 +49,25 @@ public class DatabaseTestExecutor extends TestSuiteExecutor {
 		UnitTestCase inventoryItemCreateTest4 = new UnitTestCase("inventoryItemCreateTest4", "Set URL", inventoryItem1URL, UnitTestCase.TEST_TYPE_OBJECT);
 		this.testCases.add(inventoryItemCreateTest4);
 		
+		UnitTestCase inventoryItemCreateTest5 = new UnitTestCase("inventoryItemCreateTest5", "Set Stock", inventoryItem1Stock, UnitTestCase.TEST_TYPE_OBJECT);
+		this.testCases.add(inventoryItemCreateTest5);
+		
+		UnitTestCase inventoryItemCreateTest6 = new UnitTestCase("inventoryItemCreateTest6", "Set Reorder Point", inventoryItem1ReorderPoint, UnitTestCase.TEST_TYPE_OBJECT);
+		this.testCases.add(inventoryItemCreateTest6);
+		
 		UnitTestCase inventoryItemInDBTest1 = new UnitTestCase("inventoryItemInDBTest1", "Check to make sure inventory item in DB", true, UnitTestCase.TEST_TYPE_OBJECT);
 		this.testCases.add(inventoryItemInDBTest1);
 		
 		UnitTestCase inventoryItemInDBTest2 = new UnitTestCase("inventoryItemInDBTest2", "Check to make sure the version has same name we assigned ", inventoryItem1TestName, UnitTestCase.TEST_TYPE_OBJECT);
 		this.testCases.add(inventoryItemInDBTest2);
+		
+		//UnitTestCase createClientTest1 = new UnitTestCase("createClientTest1", "Creating a client", client1TestName, UnitTestCase.TEST_TYPE_OBJECT);
+		
+		UnitTestCase createStatusTest1 = new UnitTestCase("createStatusTest1", "Set name ", statusTest1Name, UnitTestCase.TEST_TYPE_OBJECT);
+		this.testCases.add(createStatusTest1);
+		
+		//UnitTestCase createStatusTest2 = new UnitTestCase("createStatusTest2", "Set color ", statusTest1Color, UnitTestCase.TEST_TYPE_OBJECT);
+		//this.testCases.add(createStatusTest2);
 		
 		
 		
@@ -61,7 +83,7 @@ public class DatabaseTestExecutor extends TestSuiteExecutor {
 		createDatabaseTest1.setActualResult(udhTest.getDatabasePrefix());
 		
 		Boolean udhClientMapExists = (udhTest.getMap(Client.class) != null);
-		createDatabaseTest2.setTestMessage("Client Map" + udhTest.getMap(Client.class) );
+		createDatabaseTest2.setTestMessage("Client Map" + udhTest.getMap(Client.class));
 		createDatabaseTest2.setActualResult(udhClientMapExists);
 		
 		InventoryItem invItem = new InventoryItem();
@@ -69,14 +91,21 @@ public class DatabaseTestExecutor extends TestSuiteExecutor {
 		invItem.setItemName(inventoryItem1TestName);
 		invItem.setItemCategory(inventoryItem1TestCategory);
 		invItem.setItemBarcode(inventoryItem1TestBarcode);
-		invItem.setItemURL("www.google.com");
-		invItem.setItemStock(12);
-		invItem.setItemReorderPoint(6);
+		invItem.setItemURL(inventoryItem1URL);
+		invItem.setItemStock(inventoryItem1Stock);
+		invItem.setItemReorderPoint(inventoryItem1ReorderPoint);
 		
+		
+		//inventory testing
 		inventoryItemCreateTest1.setActualResult(invItem.getItemName());
 		inventoryItemCreateTest2.setActualResult(invItem.getItemCategory());
 		inventoryItemCreateTest3.setActualResult(invItem.getItemBarcode());
 		inventoryItemCreateTest4.setActualResult(invItem.getItemURL());
+		inventoryItemCreateTest5.setActualResult(invItem.getItemStock());
+		inventoryItemCreateTest6.setActualResult(invItem.getItemReorderPoint());
+		
+		
+		
 		
 		udhTest.store(invItem, InventoryItem.class);
 		
@@ -89,6 +118,15 @@ public class DatabaseTestExecutor extends TestSuiteExecutor {
 		InventoryItem fromDBItem1 = udhTest.getMap(InventoryItem.class).get(inventoryItemTestKey);
 		
 		inventoryItemInDBTest2.setActualResult(fromDBItem1.getItemName());
+		
+		
+		//status testing
+		Status testStatus1 = new Status();
+		testStatus1.setStatusName(statusTest1Name);
+		//testStatus1.setStatusColor(statusTest1Color);
+		
+		createStatusTest1.setActualResult(testStatus1.getStatusName());
+				
 		
 		return true;
 	}
