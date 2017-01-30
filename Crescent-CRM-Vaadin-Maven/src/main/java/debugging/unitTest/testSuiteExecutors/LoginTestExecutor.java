f8f26741d03d3b2685e34ee516a9d7883928c810
Joshua Benton <josh.benton.ce@gmail.com>
2017-01-30 14:45:36 -0500

    UI Testing Framework Example

---
+++ Crescent-CRM-Vaadin-Maven/src/main/java/debugging/unitTest/testSuiteExecutors/LoginTestExecutor.java
@@ -0,0 +1,38 @@
+package debugging.unitTest.testSuiteExecutors;
+
+import ccrmV.LoginView;
+import ccrmV.MasterUI;
+import debugging.unitTest.TestSuiteExecutor;
+import debugging.unitTest.UnitTestCase;
+
+public class LoginTestExecutor extends TestSuiteExecutor {
+
+	LoginView lView;
+	MasterUI masterUi;
+	
+	
+	
+	public LoginTestExecutor(LoginView lView, MasterUI masterUi) {
+		super();
+		this.lView = lView;
+		this.masterUi = masterUi;
+	}
+
+
+
+	@Override
+	public Boolean runTests() {
+		
+		
+		
+		lView.userField.setValue("ccrmUser");
+		lView.passField.setValue("ccrmPass");
+		
+		lView.attemptLogin();
+		
+		UnitTestCase loginUITest1 = new UnitTestCase("loginUITest1", "Attempts to login." , true, masterUi.loggedIn, UnitTestCase.TEST_TYPE_OBJECT, this);
+		
+		return true;
+	}
+
+}
