cdfb51e8ba948dc9828feeaca4b49d9751dc8395
Joshua Benton <jcbenton@email.sc.edu>
2017-02-10 16:02:11 -0500

    V1.22 Testing And Profile pictures. [Dual Push]

--- Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/ccrmV/MasterUI.java
@@ -52,7 +52,7 @@ public class MasterUI extends UI {
 	private static final long serialVersionUID = 1L;
 
 	public static final double versionNumber = 1.22;
-	public static final String versionDescription = " Testing and Profile Pictures";
+	public static final String versionDescription = " Testing and Profile Pictures.";
 
 	public MasterUI() {
 		// TODO Auto-generated constructor stub
