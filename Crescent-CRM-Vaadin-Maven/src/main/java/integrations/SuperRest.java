44cd89473bf99522d25751b439a6ec04db3133a1
Joshua Benton <josh.benton.ce@gmail.com>
2017-03-21 14:33:29 -0400

    added rest template

---
+++ Crescent-CRM-Vaadin-Maven/src/main/java/integrations/SuperRest.java
@@ -0,0 +1,11 @@
+package integrations;
+
+import org.springframework.web.client.RestTemplate;
+
+public class SuperRest {
+	
+	public static void connectToFirebase() {
+		RestTemplate firebaseConnection = new RestTemplate();
+	}
+
+}
