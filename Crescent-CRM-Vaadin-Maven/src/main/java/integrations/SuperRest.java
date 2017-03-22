56f5b358e70920de2e11a4a10c7e0382088e77f5
Joshua Benton <jcbenton@email.sc.edu>
2017-03-22 08:13:28 -0400

    Added Authorization to http headers.

--- Crescent-CRM-Vaadin-Maven/src/main/java/integrations/SuperRest.java
+++ Crescent-CRM-Vaadin-Maven/src/main/java/integrations/SuperRest.java
@@ -20,14 +20,18 @@ public class SuperRest {
 		
 		HttpHeaders headers = new HttpHeaders();
 		
-		Map<String,String> authenticationVars = new HashMap<String,String>();
+		headers.add("Authorization","key="+ Configuration.get(Configuration.FIRE_BASE_KEY));
 		
-		authenticationVars.put("code", Configuration.get(Configuration.FIRE_BASE_KEY));
-		authenticationVars.put("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8" );//plain text
+		Map<String,String> vars = new HashMap<String,String>();
+		
+		//authenticationVars.put("code", Configuration.get(Configuration.FIRE_BASE_KEY));
+		//authenticationVars.put("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8" );//plain text
+		
+		Debugging.output("Headers: " + headers.toString(), Debugging.FIREBASE);
 		
 		Debugging.output("Auth Key: " + Configuration.get(Configuration.FIRE_BASE_KEY), Debugging.FIREBASE);
 		
-		HttpEntity<Map<String,String>> request = new HttpEntity<Map<String,String>>(authenticationVars,headers);
+		HttpEntity<Map<String,String>> request = new HttpEntity<Map<String,String>>(vars,headers);
 		
 		ResponseEntity<String> response = firebaseConnection.
 				postForEntity("https://fcm.googleapis.com/fcm/send",
